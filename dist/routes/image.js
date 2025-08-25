cb97a27348d4a1e956c2311b4f8615993edbc676
LeenShaikh <leen.aljaber123@gmail.com>
2025-08-25 18:08:35 +0300

    Setup basic Express server with app.ts and server.ts

---
+++ dist/routes/image.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.send('Image API is working');
+});
+export default router;
+//# sourceMappingURL=image.js.map
